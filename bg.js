0b50b0345c6d0766f10ab9f64c31ec58f532f24b
Mohammad Parvez Musharaf <88303363+parvez2m@users.noreply.github.com>
2022-10-02 22:39:32 +0600

    Create bg.js

---
+++ bg.js
@@ -0,0 +1,11 @@
+import React from "react";
+import css from "./Home.module.css";
+const Home = () => {
+  return (
+    <div className={css.bg}>
+        
+    </div>
+  );
+};
+
+export default Home;
